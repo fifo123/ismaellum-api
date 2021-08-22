1791dcbb3dd881789f08e56e70e0c56ac830d861
Filipe Lacerda <filipelacerda2661@gmail.com>
2021-08-22 16:18:15 -0300

    refactor: lint in new decorator

--- src/common/decorators/user-logged.decorator.ts
+++ src/common/decorators/user-logged.decorator.ts
@@ -1,8 +1,10 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const UserLogged = createParamDecorator((_: string, ctx: ExecutionContext) => {
-  const arg = ctx?.getArgs();
-  const type = ctx?.getType();
+export const UserLogged = createParamDecorator(
+  (_: string, ctx: ExecutionContext) => {
+    const arg = ctx?.getArgs();
+    const type = ctx?.getType();
 
-  return type === 'http' ? arg?.[0]?.user :  arg?.[2]?.req.user  ;
-})
+    return type === 'http' ? arg?.[0]?.user : arg?.[2]?.req.user;
+  },
+);
