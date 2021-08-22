679f45e64b3f1e254b3e2d74fba2ba3e859a1416
Filipe Lacerda <filipelacerda2661@gmail.com>
2021-08-22 16:14:52 -0300

    feat: creating custom decorator to get logged user

---
+++ src/common/decorators/user-logged.decorator.ts
@@ -0,0 +1,8 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+export const UserLogged = createParamDecorator((_: string, ctx: ExecutionContext) => {
+  const arg = ctx?.getArgs();
+  const type = ctx?.getType();
+
+  return type === 'http' ? arg?.[0]?.user :  arg?.[2]?.req.user  ;
+})
