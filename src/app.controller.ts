47a2e61f6df5574939e450048b1e38fdcb9c2933
Filipe Lacerda <filipelacerda2661@gmail.com>
2021-10-20 01:33:09 -0300

    feat: get image

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,33 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get('/healthz')
   healthz(): string {
     return 'ok';
+  }
+
+  @Get('get-image/')
+  getImagem(@Query('imagem') imagem: string, @Res() res: any) {
+    if (imagem == '') {
+      return res.sendFile('not_found.svg', { root: './assets' });
+    }
+    if (imagem == 'not_found.svg')
+      return res.sendFile('not_found.svg', { root: './assets' });
+    if (imagem.indexOf('../assets') != -1) {
+      const asset = imagem.replace('../assets/', '');
+
+      return res.sendFile(asset, { root: './assets' }, (err: any) => {
+        if (err) {
+          res.sendFile('not_found.svg', { root: './assets' });
+        }
+      });
+    }
+
+    return res.sendFile(imagem, { root: './files' }, (err: any) => {
+      if (err) {
+        res.sendFile('not_found.svg', { root: './assets' });
+      }
+    });
   }
 }
